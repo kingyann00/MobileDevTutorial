3dd8545cdb58d3a269a5f82b9e732cbfd7562248
kingyann00 <honkingyann@gmail.com>
2022-07-06 08:41:16 +0800

    Dice xml

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -18,8 +18,20 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this,"Roll!",Toast.LENGTH_SHORT).show()
 
         val randomNum = (1..6).random()
+        val diceResource = when (randomNum){
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else->R.drawable.dice_6
+
+
+        }
 
         val numberText: TextView = findViewById(R.id.numberTxt)
-        numberText.text = randomNum.toString()
-    }
+//        numberText.text = fileName.toString()
+
+        numberText.setBackgroundResource(diceResource)
+}
 }
