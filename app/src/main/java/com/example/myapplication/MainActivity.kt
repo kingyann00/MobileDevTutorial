58ddd73395965c0e1b28ee33c5b989c48df8f94d
hon <honkingyann@gmail.com>
2022-07-12 18:07:48 +0800

    qqq

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var dice_Img:ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dice_Img = findViewById(R.id.diceImg)
+
 
         val rollButton: Button = findViewById(R.id.rollBtn);
         rollButton.setOnClickListener{rollDice()}
@@ -17,7 +22,10 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice(){
         Toast.makeText(this,"Roll!",Toast.LENGTH_SHORT).show()
 
+
         val randomNum = (1..6).random()
+
+
         val diceResource = when (randomNum){
             1->R.drawable.dice_1
             2->R.drawable.dice_2
@@ -29,9 +37,11 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        val numberText: TextView = findViewById(R.id.numberTxt)
-//        numberText.text = fileName.toString()
 
-        numberText.setBackgroundResource(diceResource)
+        dice_Img.setImageResource(diceResource)
+
+        val numberText: TextView = findViewById(R.id.numberTxt)
+        val fileName = "dice_".plus(randomNum)
+        numberText.text = fileName
 }
 }
