075eba810f5f54d00326020e78c641f09e93bd45
TARUC <honkingyann@gmail.com>
2022-07-19 18:01:34 +0800

    19-7

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.myapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var dice_Img:ImageView
+    lateinit var editPlayerNameET:EditText
+    lateinit var playerName:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,7 +19,25 @@ class MainActivity : AppCompatActivity() {
 
 
         val rollButton: Button = findViewById(R.id.rollBtn);
+        val etButton: Button = findViewById(R.id.editButton);
         rollButton.setOnClickListener{rollDice()}
+        playerName = findViewById(R.id.playerName)
+        editPlayerNameET = findViewById(R.id.editPlayerNameET)
+        etButton.setOnClickListener{editPlayer(it)}
+    }
+    private  fun editPlayer(view: View){
+
+
+
+        playerName.text = editPlayerNameET.text
+        editPlayerNameET.text.clear()
+        editPlayerNameET.clearFocus()
+        playerName.setPaintFlags(View.INVISIBLE);
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
+
+
     }
     private fun rollDice(){
         Toast.makeText(this,"Roll!",Toast.LENGTH_SHORT).show()
